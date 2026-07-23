571dda66ec4a306d7193c3b073890d3559a7473f
Abhiram-C-Divakaran <abhiramcd27@gmail.com>
2026-07-23 13:40:55 +0530

    fix: import ArrowRightLeft icon

--- src/features/learn/BinaryTreePage.tsx
+++ src/features/learn/BinaryTreePage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ArrowRightLeft } from 'lucide-react';
 import DataStructurePageLayout from './components/DataStructurePageLayout';
 
 export default function BinaryTreePage() {
